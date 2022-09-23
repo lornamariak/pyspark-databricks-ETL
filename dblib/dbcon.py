b37fabaf918805c6607b193a450870f30ac41270
Lorna Maria <lorna1661maria@gmail.com>
2022-09-23 17:43:19 -0400

    Delete dbcon.py

--- dblib/dbcon.py
+++
@@ -1,9 +0,0 @@
-from databricks import sql
-
-
-connection = sql.connect(server_hostname= "adb-2380197089486732.12.azuredatabricks.net",
-                         http_path= "sql/protocolv1/o/2380197089486732/0916-171417-6r6tbrhq",
-                         access_token= "dapiee775ec9dcc06ec2998d1594cd7ce220-3"
-                        ) 
-
-cursor = connection.cursor()
