85e1fad73eeb4160efb7ecd67419dc54badaa834
Lorna Maria <lorna@Lornas-MacBook-Pro.local>
2022-10-12 13:46:24 -0400

    add commands

--- app/main.py
+++ app/main.py
@@ -1,15 +1,14 @@
-# main file for terminal ops
-# cli.py
-#!/usr/bin/python
+#!/usr/bin/env python3
+
+import click
 from dblib.selectdb import selectdb
 from dblib.querydb import query
-import click
-
+from dblib.sampler import summary
+from dblib.sampler import sampler
 
 @click.group
 def main():
     pass
-
 
 # click to select preferred database
 @main.command()
@@ -24,6 +23,23 @@ def dbselect(database):
 @click.argument("tablename")
 def tableselect(limit, tablename):
     query(tablename, limit)
+
+
+@main.command
+@click.option("--limit", prompt="Limit", help="Summarize")
+#@click.option("--tablename", prompt = "please enter the tablename")
+@click.argument("tablename")
+def summarize(tablename, limit):
+    summary(tablename, limit)
+
+
+
+@main.command
+@click.option("--limit", prompt="Limit", help="Sampler")
+@click.option("--n",prompt="Sample size", help="Sampler")
+@click.argument("tablename")
+def sample(tablename,limit,n):
+    sampler(tablename,limit,n)
 
 
 if __name__ == "__main__":
