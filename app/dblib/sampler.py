a24ec674e7ef3355f0d261efab55b83e9806f313
Lorna Maria <lorna@Lornas-MacBook-Pro.local>
2022-10-12 13:47:22 -0400

    adds functions for newcommands

---
+++ app/dblib/sampler.py
@@ -0,0 +1,50 @@
+import pandas as pd
+
+from databricks import sql
+from configparser import ConfigParser
+config = ConfigParser()
+config.read(r'./config/keys_config.cfg')
+
+
+connection = sql.connect(server_hostname= config.get("databricks", "server_hostname"),
+                         http_path= config.get("databricks", "http_path"),
+                         access_token=config.get("databricks", "access_token")
+                        ) 
+
+cursor = connection.cursor()
+
+#summary
+def summary(tablename,limit):
+    cursor.execute("USE nycdata_db;")
+    cursor.execute(f"SELECT * FROM {tablename} LIMIT {limit}")
+    result = cursor.fetchall()
+
+    df = pd.DataFrame(result)
+    if tablename == "vehicle":
+        df.columns = ["Summons number", "Plate ID", "Registration State", "Plate type","Issue Date"]
+    elif tablename == "offence":
+         df.columns = ["Summons number", "Violation Code", "Violation Location", "Violation Precinct","Violation Description"]
+    else:
+        df.columns = ["Summons number", "Vehicle type", "Vehicle make", "Vehicle Color","Violation year"]
+    print(df.describe())
+    pass
+
+#summary("vehicle",18)
+
+#sample
+def sampler(tablename,limit,n):
+    cursor.execute("USE nycdata_db;")
+    cursor.execute(f"SELECT * FROM {tablename} LIMIT {limit}")
+    result = cursor.fetchall()
+    df = pd.DataFrame(result)
+    if tablename == "vehicle":
+        df.columns = ["Summons number", "Plate ID", "Registration State", "Plate type","Issue Date"]
+    elif tablename == "offence":
+         df.columns = ["Summons number", "Violation Code", "Violation Location", "Violation Precinct","Violation Description"]
+    else:
+        df.columns = ["Summons number", "Vehicle type", "Vehicle make", "Vehicle Color","Violation year"]
+
+    print(df.sample(n))
+    pass
+#sample("vehicle",18, 6)
+
